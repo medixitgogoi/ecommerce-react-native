d517eb851f917e4697220c190a11d17a2013e2e3
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-03-18 17:44:43 +0530

    Pull commit

--- src/screens/Category.js
+++ src/screens/Category.js
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View, SafeAreaView, StatusBar, TouchableOpacity } from 'react-native';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import Icon from 'react-native-vector-icons/dist/MaterialIcons';
 import Icon2 from 'react-native-vector-icons/dist/Ionicons';
 import Modal from "react-native-modal";
+import RadioGroup from 'react-native-radio-buttons-group';
 
 const Category = ({ navigation, route }) => {
 
     const [isModalVisible, setModalVisible] = useState(false);
+    const [selectedId, setSelectedId] = useState();
 
     const toggleModal = () => {
         setModalVisible(!isModalVisible);
@@ -16,6 +18,33 @@ const Category = ({ navigation, route }) => {
         setModalVisible(true)
     }, [])
 
+    const radioButtons = useMemo(() => ([
+        {
+            id: 'M',
+            label: (
+                <Text style={{ color: "#000", marginLeft: 3, fontSize: 16, fontWeight: "500" }}>{'Men'}</Text>
+            ),
+            color: "#000",
+            size: 15,
+        },
+        {
+            id: 'W',
+            label: (
+                <Text style={{ color: "#000", marginLeft: 2, fontSize: 16, fontWeight: "500" }}>{'Women'}</Text>
+            ),
+            color: "#000",
+            size: 15,
+        },
+        {
+            id: 'C',
+            label: (
+                <Text style={{ color: "#000", marginLeft: 3, fontSize: 16, fontWeight: "500" }}>{'Children'}</Text>
+            ),
+            color: "#000",
+            size: 15,
+        },
+    ]), []);
+
     return (
         <SafeAreaView style={{ flex: 1, }}>
             <StatusBar
@@ -23,6 +52,7 @@ const Category = ({ navigation, route }) => {
                 backgroundColor="#f6f6f6"
                 barStyle="dark-content"
             />
+
             <View style={{ backgroundColor: "#f6f6f6", height: "100%" }}>
 
                 {/* header */}
@@ -44,19 +74,27 @@ const Category = ({ navigation, route }) => {
                     isVisible={isModalVisible}
                     animationIn="slideInUp"
                     animationOut="slideOutDown"
-                    style={{ backgroundColor: "#fff", height: 200, width: 200}}
+                    onBackdropPress={() => setModalVisible(false)}
+                    onSwipeComplete={() => setModalVisible(false)}
                 >
-                    <View style={{ }}>
-                        <Text>Hello!</Text>
+                    <View style={{ backgroundColor: "#fff", height: 160, width: 300, alignSelf: "center", borderRadius: 10 }}>
+                        <Text style={{color: "#000", fontWeight: "600", textAlign: "center", fontSize: 20, paddingTop: 20}}>Select category type:</Text>
+                        <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", height: "70%" }}>
+                            <RadioGroup
+                                radioButtons={radioButtons}
+                                onPress={setSelectedId}
+                                selectedId={selectedId}
+                            />
+                        </View>
 
-                        <TouchableOpacity onPress={toggleModal}>
-                            <Text style={{ textAlign: "center" }}>Hide</Text>
-                        </TouchableOpacity>
+                        {/* <TouchableOpacity onPress={toggleModal}>
+                            <Text style={{}}>Hide</Text>
+                        </TouchableOpacity> */}
+
                     </View>
                 </Modal>
 
             </View>
-
         </SafeAreaView>
     )
 }
